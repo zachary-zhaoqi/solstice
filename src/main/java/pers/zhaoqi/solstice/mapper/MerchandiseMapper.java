d593ddc18bf9990c9523759b479a4a2688823cf3
zachary.zhao <starry-zhaoqi@outlook.com>
2019-04-12 18:09:15 +0800

    新建代码生成器

--- src/main/java/pers/zhaoqi/solstice/mapper/MerchandiseMapper.java
+++
@@ -1,9 +0,0 @@
-package pers.zhaoqi.solstice.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import pers.zhaoqi.solstice.entity.Merchandise;
-
-@Mapper
-public interface MerchandiseMapper extends BaseMapper<Merchandise> {
-}
