4dba6209ea41ab141e0666b0cf5545557ec7a238
zachary.zhao <zachary.zhao@e-jlt.com>
2019-04-26 09:47:46 +0800

    重新分布包，重新生成userinfo userlogin

--- src/main/java/pers/zhaoqi/solstice/common/enums/ConstantMessage.java
+++ src/main/java/pers/zhaoqi/solstice/common/enums/ConstantMessage.java
@@ -3,4 +3,5 @@ package pers.zhaoqi.solstice.common.enums;
 public class ConstantMessage {
     public static final Integer FAIL_CODE = -500;
     public static final Integer WANT_CORRECT_INPUT = -501;
+    public static final int LOGIN_ERROR = 666;
 }
