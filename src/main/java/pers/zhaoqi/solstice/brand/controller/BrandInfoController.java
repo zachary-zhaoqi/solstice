b923e646d37406bf69ce581f5952f953bc6aa52d
赵奇|牧 <starry-zhaoqi@outlook.com>
2019-05-08 23:16:56 +0800

    获取品牌信息

--- src/main/java/pers/zhaoqi/solstice/brand/controller/BrandInfoController.java
+++ src/main/java/pers/zhaoqi/solstice/brand/controller/BrandInfoController.java
@@ -1,9 +1,19 @@
 package pers.zhaoqi.solstice.brand.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import pers.zhaoqi.solstice.brand.entity.BrandInfo;
+import pers.zhaoqi.solstice.brand.service.IBrandInfoService;
+import pers.zhaoqi.solstice.common.enums.ConstantMessage;
+import pers.zhaoqi.solstice.common.result.ActionResult;
+import pers.zhaoqi.solstice.common.result.Result;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +26,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/brand/brandInfo")
 public class BrandInfoController {
+
+    @Autowired
+    IBrandInfoService brandInfoService;
+
+    @GetMapping
+    public ActionResult getBrand(BrandInfo brandInfo){
+        QueryWrapper queryWrapper=new QueryWrapper(brandInfo);
+        List list = brandInfoService.list(queryWrapper);
+        if (list==null||list.size()==0){
+            return Result.failed(ConstantMessage.FAIL_CODE,"不存在该品牌;");
+        }else {
+            return Result.success("查询成功",list);
+        }
+    }
 
 }
