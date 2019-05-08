1dc3b36293460d6a995a2da55f376aaebb53b455
赵奇|牧 <starry-zhaoqi@outlook.com>
2019-05-08 23:18:24 +0800

    完善了是否将数据字典转换为tree

--- src/main/java/pers/zhaoqi/solstice/dictionary/controller/DataDictionaryController.java
+++ src/main/java/pers/zhaoqi/solstice/dictionary/controller/DataDictionaryController.java
@@ -2,7 +2,6 @@ package pers.zhaoqi.solstice.dictionary.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.sun.source.tree.Tree;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -17,9 +16,7 @@ import pers.zhaoqi.solstice.common.result.Result;
 import pers.zhaoqi.solstice.dictionary.dto.DataDictionaryInputDTO;
 import pers.zhaoqi.solstice.dictionary.entity.DataDictionary;
 import pers.zhaoqi.solstice.dictionary.service.IDataDictionaryService;
-import pers.zhaoqi.solstice.userlogin.service.IUserLoginService;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,26 +38,24 @@ public class DataDictionaryController {
 
     @GetMapping()
     public ActionResult QueryDataDictionary(DataDictionaryInputDTO dataDictionaryInputDTO) {
-//        todo:if (dataDictionaryInputDTO==null)
         DataDictionary dataDictionary = new DataDictionary();
         BeanUtils.copyProperties(dataDictionaryInputDTO, dataDictionary);
         QueryWrapper queryWrapper = new QueryWrapper(dataDictionary);
-        List<DataDictionary> list = null;
         try {
-            list = dataDictionaryService.listObjs(queryWrapper);
+            List<DataDictionary> list = dataDictionaryService.listObjs(queryWrapper);
+            if (dataDictionaryInputDTO.isTree()) {
+                return Result.success("返回查询到的树状数据", convertTree(list));
+            } else {
+                return Result.success("返回查询到的数据", list);
+            }
         } catch (Exception e) {
             logger.debug(e.getMessage());
-            return Result.failed(-500, "QueryDataDictionary查询出错；");
-        }
-        if (dataDictionaryInputDTO.isTree()) {
-            return Result.success("查询成功", CreatTree(list));
-        } else {
-            return Result.success("查询成功", list);
+            return Result.failed(ConstantMessage.FAIL_CODE, "QueryDataDictionary查询出错；");
         }
     }
 
     /**
-     * 根据传入的list生成一个tree
+     * 根据传入的list转化为一个tree
      * todo
      *
      * @param dataDictionaryList
@@ -68,11 +63,11 @@ public class DataDictionaryController {
      * @throws NullPointerException
      * @author 陈亮
      */
-    public List CreatTree(List<DataDictionary> dataDictionaryList) throws NullPointerException {
+    public List convertTree(List<DataDictionary> dataDictionaryList) throws NullPointerException {
         if (dataDictionaryList == null || dataDictionaryList.size() == 0) {
             throw new NullPointerException("dataDictionaryList为空！");
         } else {
-            List<DataDictionary> tree = new ArrayList<DataDictionary>();
+            List<DataDictionary> tree = new ArrayList<>();
             for (DataDictionary dataDictionary :
                     dataDictionaryList) {
                 if (dataDictionary.getParentId() == null) {
