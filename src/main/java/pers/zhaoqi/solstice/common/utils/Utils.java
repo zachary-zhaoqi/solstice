cffad258d40488df7cd2ae5ec350d5a50660676e
赵奇|牧 <starry-zhaoqi@outlook.com>
2019-05-10 01:14:23 +0800

    getClaimsForCookies FillCreate

--- src/main/java/pers/zhaoqi/solstice/common/utils/Utils.java
+++ src/main/java/pers/zhaoqi/solstice/common/utils/Utils.java
@@ -1,8 +1,40 @@
 package pers.zhaoqi.solstice.common.utils;
 
-import javax.servlet.http.Cookie;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import pers.zhaoqi.solstice.common.entity.BaseEntity;
+import pers.zhaoqi.solstice.common.enums.KeyEnums;
+import pers.zhaoqi.solstice.product.entity.ProductInfo;
+import pers.zhaoqi.solstice.userinfo.entity.UserInfo;
+import pers.zhaoqi.solstice.userinfo.service.IUserInfoService;
+import pers.zhaoqi.solstice.userlogin.jwt.JWTUtils;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@Component
 public class Utils {
+
+    private static IUserInfoService userInfoService;
+
+    @Autowired()
+    public void setUserInfoService(IUserInfoService userInfoService){
+        Utils.userInfoService=userInfoService;
+    }
+
+    public static Claims getClaimsForCookies() throws Exception {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        Cookie[] cookies = request.getCookies();
+        String token = Utils.getTokenForCookies(cookies);
+        Claims claims = JWTUtils.parseJWT(token);
+        return claims;
+    }
+
     public static String getTokenForCookies(Cookie[] cookies) {
         String token = "";
         if (cookies != null) {
@@ -14,4 +46,18 @@ public class Utils {
         }
         return token;
     }
+
+    public static void FillCreate(BaseEntity baseEntity) {
+        try {
+            Claims claims = Utils.getClaimsForCookies();
+            Integer currentUserId = (Integer) claims.get(KeyEnums.CLAIMS_USER_ID);
+            UserInfo userInfo = userInfoService.getById(currentUserId);
+            baseEntity.setCreate(userInfo.getUserId());
+            baseEntity.setCreateName(userInfo.getUserName());
+            baseEntity.setCreateTime(LocalDateTime.now());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
